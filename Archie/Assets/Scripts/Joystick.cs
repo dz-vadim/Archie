4b569608a43c501098e806304e7a012b049ff0fc
Student <student@corp.ies.org.ua>
2020-05-26 21:02:09 +0300

    Added joystick

--- Archie/Assets/Scripts/Joystick.cs
+++ Archie/Assets/Scripts/Joystick.cs
@@ -1,18 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class Joystick : MonoBehaviour
+public class Joystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    private Image _joystickBG;
+    [SerializeField]
+    private Image _joystickHendle;
+    private Vector2 _inputVector;
+    private void Start()
     {
-        
+        _joystickBG = GetComponent<Image>();
+        _joystickHendle = transform.GetChild(0).GetComponent<Image>();
+    }
+    public virtual void OnDrag(PointerEventData eventData)
+    {
+        Vector2 _touchPoint;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(_joystickBG.rectTransform,
+                                                                    eventData.position,
+                                                                    eventData.pressEventCamera,
+                                                                    out _touchPoint))
+
+        {
+            _touchPoint.x /= _joystickBG.rectTransform.sizeDelta.x;
+            _touchPoint.y /= _joystickBG.rectTransform.sizeDelta.y;
+            print(_touchPoint);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public virtual void OnPointerDown(PointerEventData eventData)
     {
-        
+        print("OnPointerDown");
+    }
+
+    public virtual void OnPointerUp(PointerEventData eventData)
+    {
+        _inputVector = Vector2.zero;
+        _joystickHendle.rectTransform.anchoredPosition = Vector2.zero;
     }
 }
